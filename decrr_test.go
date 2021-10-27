8df9913bb75718f69c738ad361c1fc45b236e567
Reinaldy Rafli <aldy505@tutanota.com>
2021-10-27 22:59:40 +0700

    test: approach of getting goroot

--- decrr_test.go
+++ decrr_test.go
@@ -3,6 +3,8 @@ package decrr_test
 import (
 	"errors"
 	"os"
+	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/aldy505/decrr"
@@ -13,11 +15,18 @@ func TestWrap(t *testing.T) {
 	w := decrr.Wrap(x)
 	wd, _ := os.Getwd()
 
+	cmd, err := exec.Command("bash", "-c", "go env | grep GOROOT").CombinedOutput()
+	if err != nil {
+		t.Error(err)
+	}
+
+	gt := strings.Replace(strings.Replace(strings.Replace(string(cmd), "GOROOT=", "", 1), "\"", "", 2), "\n", "", -1)
+
 	var expected string
 	expected += "why does a chicken cross the road?\n\n"
-	expected += "github.com/aldy505/decrr_test.TestWrap " + wd + "/decrr_test.go:13\n"
-	expected += "testing.tRunner /usr/local/go/src/testing/testing.go:1259\n"
-	expected += "runtime.goexit /usr/local/go/src/runtime/asm_amd64.s:1581"
+	expected += "github.com/aldy505/decrr_test.TestWrap " + wd + "/decrr_test.go:15\n"
+	expected += "testing.tRunner " + gt + "/src/testing/testing.go:1259\n"
+	expected += "runtime.goexit " + gt + "/src/runtime/asm_amd64.s:1581"
 
 	if w.Error() != expected {
 		t.Error("different than expected:", w.Error(), "\nexpected:", expected)
